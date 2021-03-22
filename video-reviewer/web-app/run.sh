ab9afc11dbf418baf4b38124ca68a4a5ca95623f
James Kirk <james.kirk84@gmail.com>
2021-03-22 12:34:31 +0000

    fixing video-reviewer web app bug when no videos present

--- video-reviewer/web-app/run.sh
+++ video-reviewer/web-app/run.sh
@@ -2,8 +2,10 @@
 mkdir ./static/videos/
 rm -rf ./static/videos/*
 
-# Create new links to videos on USB drive so web server can see them
-ln -s /mnt/usb/videos/*.mp4 ./static/videos/
+# Create new links to videos on USB drive so web server can see them, if folder is not empty
+if [ "$(ls -A /mnt/usb/videos)" ]; then
+  ln -s /mnt/usb/videos/*.mp4 ./static/videos/
+fi
 
 # Link the uptimelongs into static
 ln -s /home/pi/pi-trailcam/uptime-logger/uptimelog.txt ./static/
